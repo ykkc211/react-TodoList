b72430123edc7f90e069c8df7e1eb6e3215ab914
ykkc211 <rksdudgns222@gmail.com>
2020-11-27 19:21:04 +0900

    react TodoList 만들기

---
+++ Desktop/React nomadcodrs Ex1/todolist/src/App.js
@@ -0,0 +1,100 @@
+import React , { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
+
+class App extends Component {
+
+  id = 0;
+
+  state = {
+    input: '',
+    todos: [
+      // {id:0, text:'리액트 공부하기', checked: false },
+      // {id:1, text:'javascript 공부하기', checked: false }
+    ]
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value //input의 다음 바뀔 값
+    });
+  }
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+    this.setState({
+      input: '', // input 비우고
+      //concat을 이용하여 배열에 추가
+      todos: todos.concat({
+        id:this.id++,
+        text: input,
+        checked: false
+      })
+    });
+  }
+
+
+  handleKeyPress = (e) => {
+    //눌려진 키가 Enter면 handleCreate 호출
+    if(e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+  
+  handleToggle = (id) => {
+    const { todos } = this.state;
+
+    //파라미터로 받은 id를 가지고 몇번째 아이템인지 찾는다.
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index]; //선택한 객체
+
+    const nextTodos = [...todos]; //배열을 복사
+    
+
+    //기존의 값들을 복사하고, checked 값을 덮어쓰기
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked
+    }
+
+    this.setState({
+      todos: nextTodos
+    })
+
+  }
+
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
+  }
+
+
+  render() {
+    const { input , todos } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove
+    } = this;
+
+    return (
+      <TodoListTemplate form={(
+        <Form
+        value={input}
+        onKeyPress={handleKeyPress}
+        onChange={handleChange}
+        onCreate={handleCreate}
+        />
+      )}>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
+      </TodoListTemplate>
+    );
+  }
+}
+
+export default App;
